9ec028fee90a31f9bc530916e0e6ffa875d16fdf
kajalkumarii <50989308+kajalkumarii@users.noreply.github.com>
2023-05-10 16:15:54 +0200

    debug

--- KK-intrinsicBiascwCCWVpathtest.py
+++ KK-intrinsicBiascwCCWVpathtest.py
@@ -167,9 +167,9 @@ class intrinsicBiasExperiment(fishvr.experiment.Experiment):
             self.init_y1 = fishy + 0.01 * np.sin(fishHeading)
             self.init_x2 = fishx + 0.01 * np.cos(fishHeading) + 0.09 * np.cos(fishHeading + 90 * np.pi / 180)
             self.init_y2 = fishy + 0.01 * np.sin(fishHeading) + 0.09 * np.sin(fishHeading + 90 * np.pi / 180)
-            init_heading = fishHeading
+            heading = fishHeading
         else:
-            init_heading = fishHeading
+            heading = fishHeading
 
         dist_travelled = self.counter * dt * linear_speed
 
@@ -177,22 +177,22 @@ class intrinsicBiasExperiment(fishvr.experiment.Experiment):
         new_position2 = None
 
         if dist_travelled < 0.08:
-            new_position1 = np.array([self.init_x1, self.init_y1]) + dist_travelled * np.array([np.cos(init_heading), np.sin(init_heading)])
-            new_position2 = np.array([self.init_x2, self.init_y2]) + dist_travelled * np.array([np.cos(init_heading), np.sin(init_heading)])
+            new_position1 = np.array([self.init_x1, self.init_y1]) + dist_travelled * np.array([np.cos(heading), np.sin(heading)])
+            new_position2 = np.array([self.init_x2, self.init_y2]) + dist_travelled * np.array([np.cos(heading), np.sin(heading)])
 
         elif dist_travelled < 0.08 + np.pi * pathRadius:
             circle_angle = (dist_travelled - 0.08) / pathRadius
-            new_position1 = np.array([self.init_x1, self.init_y1]) + np.array([0.08 * np.cos(init_heading) + pathRadius * (np.cos(init_heading) - np.cos(init_heading + circle_angle)),
-                                                                                0.08 * np.sin(init_heading) + pathRadius * (np.sin(init_heading) - np.sin(init_heading + circle_angle))])
-            new_position2 = np.array([self.init_x2, self.init_y2]) + np.array([0.08 * np.cos(init_heading) + pathRadius * (np.cos(init_heading) - np.cos(init_heading - circle_angle)),
-                                                                                0.08 * np.sin(init_heading) + pathRadius * (np.sin(init_heading) - np.sin(init_heading - circle_angle))])
+            new_position1 = np.array([self.init_x1, self.init_y1]) + np.array([0.08 * np.cos(heading) + pathRadius * (np.cos(heading) - np.cos(heading + circle_angle)),
+                                                                                0.08 * np.sin(heading) + pathRadius * (np.sin(heading) - np.sin(heading + circle_angle))])
+            new_position2 = np.array([self.init_x2, self.init_y2]) + np.array([0.08 * np.cos(heading) + pathRadius * (np.cos(heading) - np.cos(heading - circle_angle)),
+                                                                                0.08 * np.sin(heading) + pathRadius * (np.sin(heading) - np.sin(heading - circle_angle))])
 
         elif dist_travelled < 0.08 + np.pi * pathRadius + 0.08:
-            new_position1 = np.array([self.init_x1, self.init_y1]) + (dist_travelled - np.pi * pathRadius) * np.array([np.cos(init_heading + np.pi), np.sin(init_heading + np.pi)])
-            new_position2 = np.array([self.init_x2, self.init_y2]) + (dist_travelled - np.pi * pathRadius) * np.array([np.cos(init_heading + np.pi), np.sin(init_heading + np.pi)])
+            new_position1 = np.array([self.init_x1, self.init_y1]) + (dist_travelled - np.pi * pathRadius) * np.array([np.cos(heading + np.pi), np.sin(heading + np.pi)])
+            new_position2 = np.array([self.init_x2, self.init_y2]) + (dist_travelled - np.pi * pathRadius) * np.array([np.cos(heading + np.pi), np.sin(heading + np.pi)])
 
-        self._osg_model.move_node(self._node_name1, x=new_position1[0], y=new_position1[1], z=zHeight, orientation_z=init_heading)
-        self._osg_model.move_node(self._node_name2, x=new_position2[0], y=new_position2[1], z=zHeight, orientation_z=init_heading + np.pi)
+        self._osg_model.move_node(self._node_name1, x=new_position1[0], y=new_position1[1], z=zHeight, orientation_z=heading)
+        self._osg_model.move_node(self._node_name2, x=new_position2[0], y=new_position2[1], z=zHeight, orientation_z=heading + np.pi)
 
 
         self.positions.append((new_position1, new_position2))
